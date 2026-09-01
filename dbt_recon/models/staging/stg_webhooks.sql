55f946396624cdd8c499bf55528c8eb25dc8f88c
DhanushPillay <dhanushpillay28@gmail.com>
2026-09-01 19:28:10 +0530

    feat: stg_webhooks adds is_reconciled, has_exception columns

--- dbt_recon/models/staging/stg_webhooks.sql
+++ dbt_recon/models/staging/stg_webhooks.sql
@@ -8,8 +8,17 @@ select
     amount_paise,
     gateway_status,
     timestamp_utc,
+    cast(timestamp_utc as date) as transaction_date,
     merchant_id,
     reconciliation_status,
     bank_ref_id,
-    ingested_at
+    ingested_at,
+    case
+        when reconciliation_status = 'MATCHED' then true
+        else false
+    end as is_reconciled,
+    case
+        when reconciliation_status like 'EXCEPTION%' then true
+        else false
+    end as has_exception
 from source
