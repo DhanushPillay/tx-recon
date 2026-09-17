e2bb57282626d161b45a3f71e215a9e6370e7364
DhanushPillay <dhanushpillay28@gmail.com>
2026-09-17 12:53:40 +0530

    feat: Metabase dashboard wiring + trend SQL timestamp fix

--- sql/questions/daily_trend.sql
+++ sql/questions/daily_trend.sql
@@ -1,6 +1,8 @@
 -- Daily reconciliation trend by status.
+-- substr(..,1,10): timestamp_utc mixes ISO 'T' and space formats in the wild;
+-- plain CAST(.. AS DATE) fails on both, first-10-chars parses either.
 SELECT
-    CAST(timestamp_utc AS DATE) AS day,
+    date(substr(timestamp_utc, 1, 10)) AS day,
     reconciliation_status AS status,
     COUNT(*) AS n
 FROM nessie.db.webhooks
